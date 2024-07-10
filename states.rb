4844bffc69881c998bb59adc1a24a25fabade51e
Noman Juda <155403709+Noman-Juda@users.noreply.github.com>
2024-07-10 02:27:36 +0000

    Defined Il method

---
+++ states.rb
@@ -0,0 +1,81 @@
+class States
+  attr_accessor :feedback, :rating
+
+  def initialize(feedback = "", rating = 0)
+    @feedback = feedback
+    @rating = rating
+  end
+
+  def state
+    puts "\n"
+    puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    puts "Cities of the Major States of USA"
+    puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    puts "\n"
+    loop do
+      puts "\n"
+      puts "Please type the number of State to view the information"
+      puts "1. Illinois"
+      puts "2. Indiana"
+      puts "3. Texas"
+      puts "4. Virginia"    
+      puts "5. California"
+      puts "6. Exit"
+
+      user_choice = gets.chomp.to_i
+
+      case user_choice
+      when 1
+        il
+      when 2
+        in
+      when 3
+        tx
+      when 4
+        va
+      when 5
+        ca
+      when 6
+        puts "Thank you for using this Application."
+        puts "Hope you have find useful information of your interest."
+        break
+      else
+        puts "Please enter from 1-6 options"
+      end
+    end
+  end
+
+  def il
+    puts "\n"
+    puts "Illinois is a state in the Midwestern region of the United States."
+    puts "Capital: Springfield, Population: 12.580 million"
+    puts "Choose the city to view its zip code"
+    puts "1. Chicago"
+    puts "2. Lombard"	
+    puts "3. Villa Park"
+    puts "4. Carol Stream"
+    puts "5. Downers Grove"
+
+    il_city = gets.chomp.to_i
+    case il_city
+    when 1
+      puts "\n"
+      puts "Its Zipcode is: 60601"
+    when 2
+      puts "\n"
+      puts "Its Zipcode is: 60148"
+    when 3
+      puts "\n"
+      puts "Its Zipcode is: 60181"
+    when 4
+      puts "\n"
+      puts "Its Zipcode is: 60188"
+    when 5
+      puts "\n"
+      puts "Its Zipcode is: 60515"
+    end
+    feedback
+  end
+
+  
+end
